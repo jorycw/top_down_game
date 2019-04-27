5e610e82fc105007e406d21a948b1ea05acb877a
jorycw <jorycw@cs.washington.edu>
2019-04-27 06:21:35 -0700

    update

--- player.py
+++ player.py
@@ -4,29 +4,32 @@ import projectile as prjct
 SPEED = 10
 PROJ_CD = 60
 
-class Player:
+class Player(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("resources/player.png")
-        self.x = x
-        self.y = y
+        self.rect = self.image.get_rect()
+        
+        self.rect.x = x
+        self.rect.y = y
         self.cd = 0
 
-    def get_movement(self):
+    def movement(self):
         keys_pressed = pygame.key.get_pressed()
         if keys_pressed[pygame.K_LEFT]:
-            self.x -= SPEED
+            self.rect.x -= SPEED
         if keys_pressed[pygame.K_RIGHT]:
-            self.x += SPEED
+            self.rect.x += SPEED
         if keys_pressed[pygame.K_UP]:
-            self.y -= SPEED
+            self.rect.y -= SPEED
         if keys_pressed[pygame.K_DOWN]:
-            self.y += SPEED
+            self.rect.y += SPEED
 
     def get_projectile(self, event):
         if self.cd == 0:
             self.cd = PROJ_CD
-            proj = prjct.Projectile(self.x, self.y, event)
+            proj = prjct.Projectile(self.rect, event)
             return proj
         else:
             return None
@@ -37,7 +40,6 @@ class Player:
 
 
     def update(self, screen):
-        self.get_movement()
+        self.movement()
         self.update_cd()
-
-        screen.blit(self.image, (self.x, self.y))
+        screen.blit(self.image, self.rect)
